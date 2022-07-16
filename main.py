b0beec4740d263f85ca55c79c62e5f204f54c137
Angelo Barisano <barisano@web.de>
2022-07-16 20:32:18 +0200

    test commit from vs terminal

---
+++ main.py
@@ -0,0 +1,22 @@
+# auto docstring
+# to activate type three times double quote (""") in function
+
+def add(a: int, b: int) -> int:
+    """_summary_
+
+    Args:
+        a (int): _description_
+        b (int): _description_
+
+    Returns:
+        int: _description_
+    """
+    return a + b
+
+
+import json
+import random
+
+print(random.randint(1, 100))
+
+
